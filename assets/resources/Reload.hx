382b06f8c2ae77aa388557985e3313deabf6efff
Viwu <sinconsistencia@gmail.com>
2025-10-04 23:25:54 -0500

    idfk where this came from

--- assets/resources/Reload.hx
+++ assets/resources/Reload.hx
@@ -22,27 +22,3 @@ class Reload extends Module
 		}
 	}
 }
-import flixel.FlxG;
-import modding.PolymodHandler;
-import modding.events.UpdateEvent;
-import modding.modules.Module;
-
-class Reload extends Module
-{
-        public var canReload:Bool = true;
-
-	override public function new()
-	{
-		super('reload-resource');
-	}
-
-	override function onUpdate(event:UpdateEvent)
-	{
-		super.onUpdate(event);
-
-                if (canReload && (FlxG.keys.justReleased.F4 || FlxG.keys.pressed.SHIFT && FlxG.keys.justReleased.FOUR))
-                {
-                        PolymodHandler.forceReloadAssets();
-                }
-	}
-}
